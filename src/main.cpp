5afb7005289d7682d6bc3e059ca4c4252eef26d0
LiGui <ligui2003@126.com>
2019-05-06 03:24:09 +0800

    use googletest

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,11 @@
 #include <istream>
 #include <string>
 
-#include "crypto/sha1.h"
-#include "util/strencodings.h"
-#include "test/crypto_test.hpp"
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
 
-int main()
+int main(int argc, char** argv)
 {
-    std::cout << "Hello BCEX!" << std::endl;
-
-    std::string str = "hello sha1";
-
-    CSHA1 sha1;
-    sha1.Write((unsigned char*)str.c_str(),str.length());
-
-    unsigned char buf[CSHA1::OUTPUT_SIZE];
-    memset(buf,0,CSHA1::OUTPUT_SIZE);
-
-    sha1.Finalize(buf);
-
-    std::cout << HexStr(buf,buf+CSHA1::OUTPUT_SIZE) << std::endl;
-
-    std::string cmd = "";
-
-    std::cin >> cmd;
-
-
-
-
-
-    return 0;
+    testing::InitGoogleMock(&argc,argv);
+    return RUN_ALL_TESTS();
 }
---
+++ src/test/crypto_test.cpp
@@ -0,0 +1,20 @@
+﻿#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "crypto/sha1.h"
+#include "util/strencodings.h"
+
+TEST(crypto_test,sha1)
+{
+    std::string str = "hello sha1";
+
+    CSHA1 sha1;
+    sha1.Write((unsigned char*)str.c_str(),str.length());
+
+    unsigned char buf[CSHA1::OUTPUT_SIZE];
+    memset(buf,0,CSHA1::OUTPUT_SIZE);
+
+    sha1.Finalize(buf);
+
+    EXPECT_EQ(HexStr(buf,buf+CSHA1::OUTPUT_SIZE),"64faca92dec81be17500f67d521fbd32bb3a6968");
+}
