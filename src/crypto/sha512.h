6c0ab86197bccfed0e674aa4531ff8132893787b
LiGui <ligui2003@126.com>
2019-05-08 21:14:32 +0800

    增加一些加密类

---
+++ src/crypto/sha512.h
@@ -0,0 +1,30 @@
+﻿// Copyright (c) 2014-2018 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef  BCEX_SHA512_H
+#define  BCEX_SHA512_H
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string>
+
+/** A hasher class for SHA-512. */
+class CSHA512
+{
+private:
+    uint64_t        s[8];
+    unsigned char   buf[128];
+    uint64_t        bytes;
+
+public:
+    static constexpr size_t OUTPUT_SIZE = 64;
+
+    CSHA512();
+    CSHA512&    Write(const unsigned char* data,size_t len);
+    void        Finalize(unsigned char hash[OUTPUT_SIZE]);
+    CSHA512&    Reset();
+    std::string ToString();
+};
+
+#endif // BCEX_SHA512_H
