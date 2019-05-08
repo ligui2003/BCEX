ae706038d0cf8a484d7a2300ee3b40212aad288d
LiGui <ligui2003@126.com>
2019-05-08 16:48:08 +0800

    字符串工具

--- src/test/crypto_test.cpp
+++ src/test/crypto_test.cpp
@@ -5,50 +5,21 @@
 #include "crypto/sha256.h"
 #include "util/strencodings.h"
 
+static void TestSHA1(const std::string& in,const std::string& out)
+{
+    CSHA1 sha1;
+    sha1.Write((unsigned char*)(in.c_str()),in.length());
+
+    EXPECT_EQ(sha1.ToString(),out);
+}
+
+
 TEST(crypto_test,sha1)
 {
-    std::string str;
-
-    CSHA1 sha1,sha2;
-
-    str = "hello sha1";
-    sha1.Write((unsigned char*)(str.c_str()),str.length());
-
-    str = "1111111111" \
-          "2222222222" \
-          "3333333333" \
-          "4444444444" \
-          "5555555555" \
-          "6666666666" \
-          "7777777777" \
-          "8888888888" \
-          "9999999999";
-    sha2.Write((unsigned char*)str.c_str(),str.length());
-
-    EXPECT_EQ(sha1.ToString(),"64faca92dec81be17500f67d521fbd32bb3a6968");
-    EXPECT_EQ(sha2.ToString(),"ae8435712d9a8dc2b4cc4170a8586c1357a3cb9f");
+    TestSHA1("hello sha1","64faca92dec81be17500f67d521fbd32bb3a6968");  
 }
 
 TEST(crypto_test,sha256)
 {
-    std::string str;
-
-    CSHA256 sha1,sha2;
-
-    str = "hello sha256";
-    sha1.Write((unsigned char*)(str.c_str()),str.length());
-
-    str = "1111111111" \
-          "2222222222" \
-          "3333333333" \
-          "4444444444" \
-          "5555555555" \
-          "6666666666" \
-          "7777777777" \
-          "8888888888" \
-          "9999999999";
-    sha2.Write((unsigned char*)str.c_str(),str.length());
-
-    EXPECT_EQ(sha1.ToString(),"433855b7d2b96c23a6f60e70c655eb4305e8806b682a9596a200642f947259b1");
-    EXPECT_EQ(sha2.ToString(),"c4580204e43a31ef166e3f60bd38bf2286363ca734251a42d3ed40520cd21c1d");
+  
 }
--- src/util/strencodings.h
+++ src/util/strencodings.h
@@ -3,38 +3,49 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef STRENCODINGS_H
-#define STRENCODINGS_H
+#ifndef BCEX_STRENCODINGS_H
+#define BCEX_STRENCODINGS_H
 
 #include <cstdint>
 #include <iterator>
 #include <string>
 #include <vector>
 
-// 转成16进制字符串
-template<typename T>
-std::string HexStr(const T itbegin,const T itend)
+// Used by SanitizeString()
+enum SafeChars
 {
-    std::string rv;
-    static const char hexmap[16] = { '0','1','2','3','4','5','6','7',
-                                     '8','9','a','b','c','d','e','f' };
-    rv.reserve(std::distance(itbegin,itend) * 2);
+    SAFE_CHARS_DEFAULT,     //!< The full set of allowed chars
+    SAFE_CHARS_UA_COMMENT,  //!< BIP-0014 subset
+    SAFE_CHARS_FILENAME,    //!< Chars allowed in filenames
+    SAFE_CHARS_URI,         //!< Chars allowed in URIs (RFC 3986)
+};
 
-    for(T it = itbegin;it < itend; ++it)
-    {
-        uint8_t val = static_cast<uint8_t>(*it);
-        rv.push_back(hexmap[val >> 4]);
-        rv.push_back(hexmap[val&15]);
-    }
+std::string SanitizeString(const std::string& str,int rule = SAFE_CHARS_DEFAULT);
+std::vector<unsigned char> ParseHex(const char* psz);
+std::vector<unsigned char> ParseHex(const std::string& str);
+signed char HexDigit(char c);
 
-    return rv;
-}
+bool IsHex(const std::string& str);
 
-template<typename T>
-inline std::string HexStr(const T& vch)
-{
-    return HexStr(vch.begin(),vch.end());
-}
+bool IsHexNumber(const std::string& str);
+
+std::vector<unsigned char> DecodeBase64(const char* p,bool* pf_invalid = nullptr);
+std::string DecodeBase64(const std::string& str,bool* pf_invalid = nullptr);
+std::string EncodeBase64(const unsigned char* pch,size_t len);
+std::string EncodeBase64(const std::string& str);
+
+std::vector<unsigned char> DecodeBase32(const char* p, bool* pf_invalid = nullptr);
+std::string DecodeBase32(const std::string& str, bool* pf_invalid = nullptr);
+std::string EncodeBase32(const unsigned char* pch, size_t len);
+std::string EncodeBase32(const std::string& str);
+
+void SplitHostPort(std::string in,int &portOut,std::string &hostOut);
+
+std::string i64toStr(int64_t n);
+std::string itoStr(int n);
+int64_t atoi64(const char* psz);
+int64_t atoi64(const std::string& str);
+int atoi(const std::string& str);
 
 // 是否是数字
 constexpr bool IsDigit(char c)
@@ -48,13 +59,85 @@ constexpr inline bool IsSpace(char c) noexcept
     return c == ' ' || c == '\f' || c == '\n' || c == '\r' || c == '\t' || c == '\v';
 }
 
-// 字符转小写
+bool ParseInt32(const std::string& str,int32_t *out);
+bool ParseInt64(const std::string& str,int64_t *out);
+bool ParseUInt32(const std::string& str, uint32_t *out);
+bool ParseUInt64(const std::string& str, uint64_t *out);
+bool ParseDouble(const std::string& str, double *out);
+
+template<typename T>
+std::string HexStr(const T itbegin, const T itend)
+{
+    std::string rv;
+    static const char hexmap[16] = { '0', '1', '2', '3', '4', '5', '6', '7',
+                                     '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+    rv.reserve(std::distance(itbegin, itend) * 2);
+    for(T it = itbegin; it < itend; ++it)
+    {
+        unsigned char val = (unsigned char)(*it);
+        rv.push_back(hexmap[val>>4]);
+        rv.push_back(hexmap[val&15]);
+    }
+    return rv;
+}
+
+template<typename T>
+inline std::string HexStr(const T& vch)
+{
+    return HexStr(vch.begin(), vch.end());
+}
+
+std::string FormatParagraph(const std::string& in, size_t width = 79, size_t indent = 0);
+
+
+template <typename T>
+bool TimingResistantEqual(const T& a, const T& b)
+{
+    if (b.size() == 0) return a.size() == 0;
+    size_t accumulator = a.size() ^ b.size();
+    for (size_t i = 0; i < a.size(); i++)
+        accumulator |= a[i] ^ b[i%b.size()];
+    return accumulator == 0;
+}
+
+bool ParseFixedPoint(const std::string &val, int decimals, int64_t *amount_out);
+
+template<int frombits, int tobits, bool pad, typename O, typename I>
+bool ConvertBits(const O& outfn, I it, I end) {
+    size_t acc = 0;
+    size_t bits = 0;
+    constexpr size_t maxv = (1 << tobits) - 1;
+    constexpr size_t max_acc = (1 << (frombits + tobits - 1)) - 1;
+    while (it != end) {
+        acc = ((acc << frombits) | *it) & max_acc;
+        bits += frombits;
+        while (bits >= tobits) {
+            bits -= tobits;
+            outfn((acc >> bits) & maxv);
+        }
+        ++it;
+    }
+    if (pad) {
+        if (bits) outfn((acc << (tobits - bits)) & maxv);
+    } else if (bits >= frombits || ((acc << (tobits - bits)) & maxv)) {
+        return false;
+    }
+    return true;
+}
+
 constexpr char ToLower(char c)
 {
     return (c >= 'A' && c <= 'Z' ? (c - 'A') + 'a' : c);
 }
 
-uint8_t HexDigit(char c);
+constexpr char ToUpper(char c)
+{
+    return (c >= 'a' && c <= 'z' ? (c - 'a') + 'A' : c);
+}
+
+void Downcase(std::string& str);
+
+std::string Capitalize(std::string str);
 
 
-#endif // STRENCODINGS_H
+#endif // BCEX_STRENCODINGS_H
