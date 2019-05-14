2a9d2d29a2618845e9df1cc6b1245207969646fa
LiGui <ligui2003@126.com>
2019-05-15 04:37:44 +0800

    libevebt支持

---
+++ src/util/url.h
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2015-2018 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BCEX_URL_H
+#define BCEX_URL_H
+
+#include <string>
+
+std::string urlDecode(const std::string& urlEncoded);
+
+#endif // BCEX_URL_H
